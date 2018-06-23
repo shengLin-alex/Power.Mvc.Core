1338edc99cbc35e8a00967308038d08ba389f379
DESKTOP-STF9L29\Sheng-Wei Lin <a182820998@gmail.com>
2018-06-23 20:40:19 +0800

    [開發] 調整 MySql.EntityFrameworkCore 的套件，改用 Pomelo，Oracle 是垃圾

--- src/Power.Mvc.Core/Power.Repository.EFCore/DbContextFactory/Abstraction/IMySqlDbContextFactory.cs
+++ src/Power.Mvc.Core/Power.Repository.EFCore/DbContextFactory/Abstraction/IMySqlDbContextFactory.cs
@@ -1,12 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
-
-namespace Power.Repository.EFCore
+﻿namespace Power.Repository.EFCore
 {
     /// <summary>
     /// MySqlDbContext 工廠介面
     /// </summary>
     /// <typeparam name="TContext">DbContext 實際型別</typeparam>
-    public interface IMySqlDbContextFactory<out TContext> : IDbContextFactory<TContext> where TContext : DbContext
+    public interface IMySqlDbContextFactory<out TContext> : IDbContextFactory<TContext> where TContext : MySqlDbContextBase
     {
     }
 }
--- src/Power.Mvc.Core/Power.Repository.EFCore/DbContextFactory/Abstraction/IPostgreSqlDbContextFactory.cs
+++ src/Power.Mvc.Core/Power.Repository.EFCore/DbContextFactory/Abstraction/IPostgreSqlDbContextFactory.cs
@@ -1,12 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
-
-namespace Power.Repository.EFCore
+﻿namespace Power.Repository.EFCore
 {
     /// <summary>
     /// PostgreSqlDbContext 工廠介面
     /// </summary>
     /// <typeparam name="TContext">DbContext 實際型別</typeparam>
-    public interface IPostgreSqlDbContextFactory<out TContext> : IDbContextFactory<TContext> where TContext : DbContext
+    public interface IPostgreSqlDbContextFactory<out TContext> : IDbContextFactory<TContext> where TContext : PostgreSqlDbContextBase
     {
     }
 }
